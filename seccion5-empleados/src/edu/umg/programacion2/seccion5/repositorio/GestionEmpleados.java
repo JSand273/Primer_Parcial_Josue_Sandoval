c945540aade9a4aaedc0f829f4c89183e9c0fe47
Josue <jsandovalm14@miumg.edu.gt>
2026-08-13 21:10:40 -0600

    Resolucion de tercer ejercicio practico

--- seccion5-empleados/src/edu/umg/programacion2/seccion5/repositorio/GestionEmpleados.java
+++ seccion5-empleados/src/edu/umg/programacion2/seccion5/repositorio/GestionEmpleados.java
@@ -171,9 +171,37 @@ public class GestionEmpleados {
 	 * </ul>
 	 */
 	public void mostrarConteoPorTipo() {
-		// TODO: completar
+		if (cantidad == 0) {
+			return;
+		}
+		String[] tipos = new String[cantidad];
+		int[] conteos = new int[cantidad];
+		int tiposUsados = 0;
+		
+		for (int i = 0; i < cantidad; i++) {
+			String tipoActual = empleados[i].getTipo();
+			boolean encontrado = false;
+			
+			for (int j = 0; j < tiposUsados; j++) {
+				
+				if (tipos[j].equals(tipoActual)) {
+					conteos[j]++;
+					encontrado = true;
+					break;
+				}
+			}
+			
+			if (!encontrado) {
+				tipos[tiposUsados] = tipoActual;
+				conteos[tiposUsados] = 1;
+				tiposUsados++;
+			}
+		}
+		
+		for (int j = 0; j < tiposUsados; j++) {
+			System.out.println(tipos[j] + ": " + conteos[j]);
+		}
 	}
-
 	/**
 	 * RETO OPCIONAL (10 pts extra)
 	 * <p>
